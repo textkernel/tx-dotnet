e7e61f0e5455ae89a34a1dba319bce660f96dee1
JW Wesson <dev4@sovren.com>
2020-11-29 16:41:35 -0600

    better check on empty byte[]

--- src/Sovren.SDK.Tests/IntegrationTests/ParsingTests.cs
+++ src/Sovren.SDK.Tests/IntegrationTests/ParsingTests.cs
@@ -18,7 +18,7 @@ namespace Sovren.SDK.Tests.IntegrationTests
             get
             {
                 yield return new TestCaseData(null, typeof(ArgumentNullException));
-                yield return new TestCaseData(new Models.Document(new byte[0], DateTime.Now), typeof(SovrenException));
+                yield return new TestCaseData(new Models.Document(new byte[1], DateTime.Now), typeof(SovrenException));
             }
         }
 
