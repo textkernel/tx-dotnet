6742db9571123b7ea0853a20f346c311211063fd
JW Wesson <dev4@sovren.com>
2020-12-03 16:24:57 -0600

    add copyrights

--- src/Sovren.SDK.Tests/IntegrationTests/GeocodingTests.cs
+++ src/Sovren.SDK.Tests/IntegrationTests/GeocodingTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+// Copyright © 2020 Sovren Group, Inc. All rights reserved.
+// This file is provided for use by, or on behalf of, Sovren licensees
+// within the terms of their license of Sovren products or Sovren customers
+// within the Terms of Service pertaining to the Sovren SaaS products.
+
+using NUnit.Framework;
 using Sovren.Models.API.Geocoding;
 using Sovren.Models.API.Indexes;
 using Sovren.Models.Matching;
