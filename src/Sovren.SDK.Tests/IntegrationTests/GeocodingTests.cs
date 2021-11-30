e305ca8faf3321f86acebf586227727b8ff2fcb5
James Parks <dev2@sovren.com>
2021-11-30 15:15:45 -0600

    add unit tests for replacing address

--- src/Sovren.SDK.Tests/IntegrationTests/GeocodingTests.cs
+++ src/Sovren.SDK.Tests/IntegrationTests/GeocodingTests.cs
@@ -8,6 +8,10 @@ using Sovren.Models.API.Geocoding;
 using Sovren.Models.API.Indexes;
 using Sovren.Models.Matching;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using Sovren.Models.API.Matching.Request;
+using Sovren.Models.API.Matching;
 
 namespace Sovren.SDK.Tests.IntegrationTests
 {
@@ -182,6 +186,146 @@ namespace Sovren.SDK.Tests.IntegrationTests
                 Assert.DoesNotThrowAsync(async () => {
                     await Client.GetJob(indexId, documentId);
                 });
+            }
+            finally
+            {
+                await CleanUpIndex(indexId);
+            }
+
+            await Task.CompletedTask;
+        }
+
+        [Test]
+        public async Task TestResumeGeocodeIndexReplaceAddress()
+        {
+            string indexId = "SDK-resume-" + nameof(TestResumeGeocodeIndexReplaceAddress);
+            string documentId = "1";
+
+            try
+            {
+                await Client.CreateIndex(IndexType.Resume, indexId);
+
+                IndexSingleDocumentInfo indexingOptions = new IndexSingleDocumentInfo
+                {
+                    IndexId = indexId,
+                    DocumentId = documentId
+                };
+
+                GeocodeCredentials geocodeCredentials = new GeocodeCredentials() { 
+                    Provider = GeocodeProvider.None
+                };
+
+                Address address = new Address()
+                {
+                    Municipality = "Dallas",
+                    Region = "TX",
+                    CountryCode = "US",
+                    PostalCode = "75214"
+                };
+
+                GeocodeAndIndexResumeResponse response = await Client.GeocodeAndIndex(TestParsedResumeWithAddress, indexingOptions, address, geocodeCredentials);
+
+                Assert.Multiple(() => {
+                    Assert.AreEqual(0m, response.Info.TransactionCost);
+                    Assert.AreEqual(address.CountryCode, response.Value.ResumeData.ContactInformation.Location.CountryCode);
+                    Assert.AreEqual(address.Region, response.Value.ResumeData.ContactInformation.Location.Regions.FirstOrDefault());
+                    Assert.AreEqual(address.Municipality, response.Value.ResumeData.ContactInformation.Location.Municipality);
+                    Assert.AreEqual(address.PostalCode, response.Value.ResumeData.ContactInformation.Location.PostalCode);
+                });
+
+                Assert.DoesNotThrowAsync(async () => {
+                    await Client.GetResume(indexId, documentId);
+                });
+
+                SearchResponse searchResponse = await Client.Search(new[] { indexId }, new FilterCriteria()
+                {
+                    LocationCriteria = new LocationCriteria()
+                    {
+                        Locations = new List<FilterLocation>()
+                            {
+                                new FilterLocation()
+                                {
+                                    CountryCode = address.CountryCode,
+                                    Municipality = address.Municipality,
+                                    PostalCode = address.PostalCode,
+                                    Region = address.Region
+                                }
+                            }
+                    }
+                });
+
+                Assert.AreEqual(1, searchResponse.Value.CurrentCount);
+            }
+            finally
+            {
+                await CleanUpIndex(indexId);
+            }
+
+            await Task.CompletedTask;
+        }
+
+
+        [Test]
+        public async Task TestJobGeocodeIndexReplaceAddress()
+        {
+            string indexId = "SDK-job-" + nameof(TestJobGeocodeIndexReplaceAddress);
+            string documentId = "1";
+
+            try
+            {
+                await Client.CreateIndex(IndexType.Job, indexId);
+
+                IndexSingleDocumentInfo indexingOptions = new IndexSingleDocumentInfo
+                {
+                    IndexId = indexId,
+                    DocumentId = documentId
+                };
+
+                GeocodeCredentials geocodeCredentials = new GeocodeCredentials()
+                {
+                    Provider = GeocodeProvider.None
+                };
+
+                Address address = new Address()
+                {
+                    Municipality = "Dallas",
+                    Region = "TX",
+                    CountryCode = "US",
+                    PostalCode = "75214"
+                };
+
+                GeocodeAndIndexJobResponse response = await Client.GeocodeAndIndex(TestParsedJobWithAddress, indexingOptions, address, geocodeCredentials);
+
+                Assert.Multiple(() => {
+                    Assert.AreEqual(0m, response.Info.TransactionCost);
+                    Assert.AreEqual(address.CountryCode, response.Value.JobData.CurrentLocation.CountryCode);
+                    Assert.AreEqual(address.Region, response.Value.JobData.CurrentLocation.Regions.FirstOrDefault());
+                    Assert.AreEqual(address.Municipality, response.Value.JobData.CurrentLocation.Municipality);
+                    Assert.AreEqual(address.PostalCode, response.Value.JobData.CurrentLocation.PostalCode);
+                });
+
+                Assert.DoesNotThrowAsync(async () => {
+                    await Client.GetJob(indexId, documentId);
+                });
+
+                SearchResponse searchResponse = await Client.Search(new[] { indexId }, new FilterCriteria()
+                {
+                    LocationCriteria = new LocationCriteria()
+                    {
+                        Locations = new List<FilterLocation>()
+                            {
+                                new FilterLocation()
+                                {
+                                    CountryCode = address.CountryCode,
+                                    Municipality = address.Municipality,
+                                    PostalCode = address.PostalCode,
+                                    Region = address.Region
+                                }
+                            }
+                    }
+                });
+
+                Assert.AreEqual(1, searchResponse.Value.CurrentCount);
             }
             finally
             {
