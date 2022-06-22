225d38af639d37090efb27ac7400edc98e4887fc
JW Wesson <wesson@textkernel.nl>
2022-06-22 14:37:12 -0500

    clean up some documentation and move some properties

--- src/Sovren.SDK/Models/API/Matching/Request/SearchMatchRequestBase.cs
+++ src/Sovren.SDK/Models/API/Matching/Request/SearchMatchRequestBase.cs
@@ -4,6 +4,7 @@
 // within the Terms of Service pertaining to the Sovren SaaS products.
 
 using System.Collections.Generic;
+using Sovren.Models.API.Parsing;
 
 namespace Sovren.Models.API.Matching.Request
 {
@@ -18,18 +19,22 @@ namespace Sovren.Models.API.Matching.Request
         public bool PositionTitlesMustHaveAnExactMatch { get; set; }
 
         /// <summary>
-        /// Normalize the first three job titles specified in FilterCriteria.JobTitles and automatically include them in the query. Additional <a href="https://sovren.com/technical-specs/latest/rest-api/overview/#transaction-cost">charges apply</a>.
+        /// Normalize the first three job titles specified in FilterCriteria.JobTitles and automatically include them in the query
+        /// (<see href="https://sovren.com/technical-specs/latest/rest-api/overview/#transaction-cost">additional charges apply</see>).
         /// <br/><br/>
-        /// You will only benefit from using this parameter if the data in your index was parsed with <a href="https://sovren.com/technical-specs/latest/rest-api/resume-parser/api/?h=ProfessionsSettings">Professions Normalization</a> enabled.
+        /// You will only benefit from using this parameter if the data in your index was parsed with <see cref="ProfessionsSettings.Normalize"/> enabled.
         /// <br/><br/>
-        /// Normalized job titles help identify more matches by looking beyond the exact job title. Normalization uses lists of synonyms behind the scenes. For example, a search for "HR Advisor" will also return results for "Human Resources Consultant".
+        /// Normalized job titles help identify more matches by looking beyond the exact job title. Normalization uses lists 
+        /// of synonyms behind the scenes. For example, a search for "HR Advisor" will also return results for "Human Resources Consultant".
         /// <br/><br/>
-        /// When matching, the normalized job title is automatically included in the query if the data in your index was parsed with <a href="https://sovren.com/technical-specs/latest/rest-api/resume-parser/api/?h=ProfessionsSettings">Professions Normalization</a> enabled.
+        /// When matching, the normalized job title is automatically included in the query if the data in your index was 
+        /// parsed with <see cref="ProfessionsSettings.Normalize"/> enabled.
         /// </summary>
         public bool NormalizeJobTitles { get; set; }
         
         /// <summary>
-        /// Specify the language (ISO 639-1 code) of the Job Title to be normalized. This defaults to <code>en</code>. See <a href="https://developer.textkernel.com/Professions/master/">list of supoprted languages.</a>
+        /// Specify the language (ISO 639-1 code) of the Job Title to be normalized. This defaults to "en". 
+        /// See <see href="https://developer.textkernel.com/Professions/master/">list of supoprted languages.</see>
         /// </summary>
         public string NormalizeJobTitlesLanguage { get; set; }
     }
--- src/Sovren.SDK/Models/API/Parsing/ParseOptions.cs
+++ src/Sovren.SDK/Models/API/Parsing/ParseOptions.cs
@@ -6,6 +6,9 @@
 using Sovren.Models.API.Geocoding;
 using Sovren.Models.API.Indexes;
 using System.Collections.Generic;
+using Sovren.Models.Resume.Skills;
+using Sovren.Models.API.Matching.Request;
+using System;
 
 namespace Sovren.Models.API.Parsing
 {
@@ -28,13 +31,25 @@ namespace Sovren.Models.API.Parsing
         /// If you want to use custom skills lists during parsing, set those here. This not a recommended feature for most customers.
         /// For more information, reach out to support@sovren.com
         /// </summary>
+        [Obsolete("You should use the V2 skills taxonomy instead.")]
         public List<string> SkillsData { get; set; }
 
         /// <summary>
         /// If you want to use custom normalizations during parsing, set those here. This not a recommended feature for most customers.
         /// For more information, reach out to support@sovren.com
         /// </summary>
+        [Obsolete]
         public string NormalizerData { get; set; }
+
+        /// <summary>
+        /// Enable skills normalization and enhanced candidate summarization, and specify the version of the skills taxonomy for this parsing transaction.
+        /// </summary> 
+        public SkillsSettings SkillsSettings { get; set; }
+
+        /// <summary>
+        /// Enable normalization of job titles using our proprietary taxonomy and international standards.
+        /// </summary>
+        public ProfessionsSettings ProfessionsSettings { get; set; }
 
         //********************************
         //IF YOU ADD ANY PARAMS HERE BE SURE TO ADD THEM IN THE DEEP COPY INSIDE ParseRequest.ctor() !!
@@ -80,16 +95,6 @@ namespace Sovren.Models.API.Parsing
         /// </summary>
         public IndexSingleDocumentInfo IndexingOptions { get; set; }
 
-        /// <summary>
-        /// Enable skills normalization and enhanced candidate summarization, and specify the version of the skills taxonomy for this parsing transaction.
-        /// </summary> 
-        public SkillsSettings SkillsSettings { get; set; }
-
-        /// <summary>
-        /// Enable normalization of job titles using our proprietary taxonomy and international standards.
-        /// </summary>
-        public ProfessionsSettings ProfessionsSettings { get; set; }
-
         //********************************
         //IF YOU ADD ANY PARAMS HERE BE SURE TO ADD THEM IN THE DEEP COPY INSIDE ParseRequest.ctor() !!
         //********************************
@@ -101,31 +106,33 @@ namespace Sovren.Models.API.Parsing
     public class SkillsSettings
     {
         /// <summary>
-        /// When true:
-        /// <ul>
-        ///     <li>raw skills will be normalized. These will be output under Value.ResumeData.Skills.Normalized.</li> <a href="https://www.textkernel.com/skills/">Read more</a> about the benefits of using a skills taxonomy.
-        ///     <li>An enhanced candidate summary is generated, leveraging the taxonomy structure to relate skills to profession groups.</li>
-        /// </ul>
-        /// When using TaxonomyVersion V2 (see below), additional <a href="https://www.sovren.com/technical-specs/latest/rest-api/overview/#transaction-cost">charges apply</a>. when normalization is enabled.
-        /// <br/><br/>
-        /// When you have access to TaxonomyVersion V1, and did not set the taxonomy to V2 explicitly (see below), normalization is enabled by default and the candidate summary is generated using the V1 taxonomy structure.
+        /// When <see langword="true"/>:
+        /// <br/>- Raw skills will be normalized. These will be output under <see cref="SkillsOutput.Normalized"/>.
+        /// <br/>- An enhanced candidate summary is generated, leveraging the taxonomy structure to relate skills with profession groups.
+        /// <br/>- When <see cref="TaxonomyVersion"/> is set to (or defaults to) "V2", 
+        /// <see href="https://www.sovren.com/technical-specs/latest/rest-api/overview/#transaction-cost">additional charges apply</see>.
+        /// <br/>
+        /// <br/>
+        /// <b>This setting has no effect when <see cref="TaxonomyVersion"/> is set to (or defaults to) "V1".</b>
         /// </summary>
         public bool Normalize {get;set;}
 
         /// <summary>
-        /// Specifies the version of the skills taxonomy to use. Defaults to V2, unless your account has access to V1. If you have access to V1, use v2 as the value for this property to explicitly set V2.
-        /// <br/><br/>
-        /// <strong>V1 is deprecated</strong> and will be removed in a future release.
-        /// <br/><br/>
+        /// Specifies the version of the skills taxonomy to use. One of:
+        /// <br/>"V1" - <b>(DEPRECATED)</b> This is the default for old accounts. Will be removed in a future release.
+        /// <br/>"V2" - This is the default for new accounts, and must be explicitly set if you have access to V1 and V2.
+        /// <br/>
+        /// <br/>
         /// Benefits of V2 include:
-        /// <ul>
-        ///     <li>2x larger skills taxonomy, updated frequently based on real-world data</li>
-        ///     <li>15-40% higher accuracy of extracted skills</li>
-        ///     <li>Better clustering of skill synonyms</li>
-        ///     <li>Distinguish skill types (IT / Professional / Soft)</li>
-        ///     <li>Improved candidate summary</li>
-        ///     <li>Compatibility with the taxonomy used in Textkernel's <a href="https://www.textkernel.com/solution/data-enrichment-apis/">Data Enrichment APIs</a> and <a href="https://www.jobfeed.com/">Jobfeed</a>, enabling standardization of taxonomies across all of your data and benchmarking against jobs posted online.</li>
-        /// </ul>
+        /// <br/>- 2x larger skills taxonomy, updated frequently based on real-world data.
+        /// <br/>- 15-40% higher accuracy of extracted skills.
+        /// <br/>- Better clustering of skill synonyms.
+        /// <br/>- Distinguish skill types (IT / Professional / Soft).
+        /// <br/>- Improved candidate summary.
+        /// <br/>- Compatibility with the taxonomy used in Textkernel's 
+        /// <see href="https://www.textkernel.com/solution/data-enrichment-apis/">Data Enrichment APIs</see> and 
+        /// <see href="https://www.jobfeed.com/">Jobfeed</see>, enabling standardization of taxonomies across all of 
+        /// your data and benchmarking against jobs posted online.
         /// </summary>
         public string TaxonomyVersion {get;set;}
     }
@@ -136,17 +143,22 @@ namespace Sovren.Models.API.Parsing
     public class ProfessionsSettings
     {
         /// <summary>
-        /// When true, the most recent 3 job titles will be normalized. This includes a proprietary value from our profession taxonomy, plus ONET and ISCO mappings. <a href="https://www.textkernel.com/professions-data-enrichment-api/">Read more</a> about the benefits of using a professions taxonomy.
+        /// When true, the most recent 3 job titles will be normalized. This includes a proprietary value from our profession 
+        /// taxonomy, plus ONET and ISCO mappings. <see href="https://www.textkernel.com/professions-data-enrichment-api/">Read more</see> 
+        /// about the benefits of using a professions taxonomy.
         /// <br/><br/>
-        /// When enabling professions normalization, additional <a href="https://www.sovren.com/technical-specs/latest/rest-api/overview/#transaction-cost">charges apply</a>.
+        /// When enabling professions normalization, 
+        /// <see href="https://www.sovren.com/technical-specs/latest/rest-api/overview/#transaction-cost">additional charges apply</see>.
         /// <br/><br/>
-        /// The following languages are supported: English, Chinese (Simplified), Dutch, French, German, Italian, Polish, Portuguese, and Spanish. For documents in other languages, no normalized values will be returned.
+        /// The following languages are supported: English, Chinese (Simplified), Dutch, French, German, Italian, Polish, Portuguese, 
+        /// and Spanish. For documents in other languages, no normalized values will be returned.
         /// <br/><br/>
-        /// For Sovren AI Matching, normalized professions are automatically indexed and used when profession normalization is enabled during parsing 
-        /// (through <a href="https://www.sovren.com/technical-specs/latest/rest-api/resume-parser/api/">IndexingOptions</a>). To leverage profession normalization for user-created searches, 
-        /// enable <a href="https://www.sovren.com/technical-specs/latest/rest-api/ai-matching/querying-api/search/?h=Settings.NormalizeJobTitles">profession normalization at query time</a>.
+        /// For Sovren AI Matching, normalized professions are automatically indexed and used if enabled. To also leverage profession 
+        /// normalization for user-created searches, enable <see cref="SearchMatchSettings.NormalizeJobTitles"/>.
         /// <br/><br/>
-        /// The profession taxonomy and the mappings are compatible with the taxonomies used in Textkernel's <a href="https://www.textkernel.com/solution/data-enrichment-apis/">Data Enrichment APIs</a> and <a href="https://www.jobfeed.com/">Jobfeed</a>, 
+        /// The profession taxonomy and the mappings are compatible with the taxonomies used in Textkernel's 
+        /// <see href="https://www.textkernel.com/solution/data-enrichment-apis/">Data Enrichment APIs</see> and 
+        /// <see href="https://www.jobfeed.com/">Jobfeed</see>, 
         /// enabling standardization of taxonomies across all of your data and benchmarking against jobs posted online.
         /// </summary>
         public bool Normalize {get;set;}
