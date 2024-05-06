8b2a302ea16ece3f74dfc9f252ade7d340f32d90
JW Wesson <wesson@textkernel.nl>
2024-05-06 15:17:45 -0500

    change a few links

--- src/Textkernel.Tx.SDK/Models/API/Parsing/ParseOptions.cs
+++ src/Textkernel.Tx.SDK/Models/API/Parsing/ParseOptions.cs
@@ -109,7 +109,7 @@ namespace Textkernel.Tx.Models.API.Parsing
 
         /// <summary>
         /// Only used for resumes. Custom requests to ask during parsing. 
-        /// See the <see href="https://developer.textkernel.com/tx-platform/v10/resume-parser/overview/llm-parser/#flex-requests">overview documentation</see> for more details.
+        /// See the <see href="https://developer.textkernel.com/tx-platform/v10/resume-parser/overview/flexrequests">overview documentation</see> for more details.
         /// <see href="https://developer.textkernel.com/tx-platform/v10/overview/#transaction-cost">Additional charges</see> will apply.
         /// </summary>
         public List<FlexRequest> FlexRequests { get; set; }
